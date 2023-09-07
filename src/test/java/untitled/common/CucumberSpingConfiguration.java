4d315edacd2a78c267d61adc03d410840df765f6
sooheon45 <54785805+sooheon45@users.noreply.github.com>
2023-09-07 13:50:25 +0900

    commit

--- src/test/java/untitled/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package untitled.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import untitled.BoundedContext353Application;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { BoundedContext353Application.class })
-public class CucumberSpingConfiguration {}
